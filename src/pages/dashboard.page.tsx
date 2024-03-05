0191d6718f1e10011fccf77db2745fd43afc9908
iaomar <izatshoev@protonmail.com>
2024-03-05 17:01:40 +0500

    Add navigation links to dashboard page

--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -1,5 +1,20 @@
 function DashboardPage() {
-  return <>Dashboard</>;
+  return (
+    <>
+      Dashboard page
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "10px",
+          marginTop: "20px",
+        }}
+      >
+        <a href={`/questions`}>Questions</a>
+        <a href={`/add-question`}>Add Question</a>
+      </div>
+    </>
+  );
 }
 
 export { DashboardPage };
